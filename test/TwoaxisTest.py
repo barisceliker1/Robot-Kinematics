686e61502e8370275097d07832ef30949f987904
barış çeliker <36998513+barisceliker1@users.noreply.github.com>
2023-04-19 13:05:46 +0300

    Test Files

---
+++ test/TwoaxisTest.py
@@ -0,0 +1,4 @@
+import sys
+sys.path.append('..')
+from axis2 import axis2
+axis2(3,5,20,40,30,50)
---
+++ test/jacobianTest.py
@@ -0,0 +1,4 @@
+import sys
+sys.path.append('..')
+from jacobian import jacobian
+print(jacobian(30,40,2,3))
